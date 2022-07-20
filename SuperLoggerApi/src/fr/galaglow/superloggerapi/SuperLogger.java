007315a96c9201c44973aaed8cbfbe0e2a96ed82
SorenDeveloppement <sorenel@yahoo.com>
2022-07-20 18:28:56 +0200

    V2 of SuperLoggerApi

--- SuperLoggerApi/src/fr/galaglow/superloggerapi/SuperLogger.java
+++ SuperLoggerApi/src/fr/galaglow/superloggerapi/SuperLogger.java
@@ -1,18 +1,30 @@
 package fr.galaglow.superloggerapi;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+import fr.galaglow.superloggerapi.utils.AnsiConsoleTextColors;
 import fr.galaglow.superloggerapi.utils.SurroundKey;
 
 public class SuperLogger {
 	
-	public String prefix;
+	public String prefix, args, path;
 	public SurroundKey surroundKey;
-	public String args;
+	public AnsiConsoleTextColors aColor;
 	
-	public  SuperLogger(String prefix, SurroundKey surroundKey,  String args) {
+	public static final String FILE_EXTENSION = ".slog";
+	
+	public  SuperLogger(String prefix, SurroundKey surroundKey,  String args, AnsiConsoleTextColors aColor, String path) {
 		
 		this.prefix = prefix;
 		this.surroundKey = surroundKey;
 		this.args = args;
+		this.aColor = aColor;
+		this.path = path;
+		
+		System.out.println("Thanks for using SuperLoggerApi !");
 		
 	}
 	
@@ -30,20 +42,55 @@ public class SuperLogger {
 		String doublePoint = "::";
 		
 		if (surroundKey.equals(SurroundKey.BACK_SLASH)) {
-			System.out.println(backSlash + prefix + backSlash + args);
+			System.out.println(this.aColor + backSlash + prefix + backSlash + args + "\u001B[0m");
+			logInFile(backSlash, backSlash);
 		} else if (surroundKey.equals(SurroundKey.SLASH)) {
-			System.out.println(slash + prefix + slash + args);
+			System.out.println(this.aColor + slash + prefix + slash + args + "\u001B[0m");
+			logInFile(slash, slash);
 		} else if (surroundKey.equals(SurroundKey.BAR)) {
-			System.out.println(bar + prefix + bar + args);
+			System.out.println(this.aColor + bar + prefix + bar + args + "\u001B[0m");
+			logInFile(bar, bar);
 		} else if (surroundKey.equals(SurroundKey.PARENTHESES)) {
-			System.out.println(openedPar + prefix + closedPar + args);
+			System.out.println(this.aColor + openedPar + prefix + closedPar + args + "\u001B[0m");
+			logInFile(openedPar, closedPar);
 		}  else if (surroundKey.equals(SurroundKey.HOOKS)) {
-			System.out.println(openedHooks + prefix + closedHooks + args);
+			System.out.println(this.aColor + openedHooks + prefix + closedHooks + args + "\u001B[0m");
+			logInFile(openedHooks, closedHooks);
 		} else if (surroundKey.equals(SurroundKey.HUGS)) {
-			System.out.println(openedHugs + prefix + closedHugs + args);
+			System.out.println(this.aColor + openedHugs + prefix + closedHugs + args + "\u001B[0m");
+			logInFile(openedHugs, closedHugs);
 		} else {
-			System.out.println(doublePoint + prefix + doublePoint + args);
+			System.out.println(this.aColor + doublePoint + prefix + doublePoint + args + "\u001B[0m");
+			logInFile(doublePoint, doublePoint);
 		}
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void logInFile(String sKey1, String sKey2) {
+		
+		Date date = new Date();
+		
+		 final File dir = new File(this.path + date.getDay() + "-" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + FILE_EXTENSION);
+		
+		 try {
+		      if (dir.createNewFile()) {
+		    	  System.out.println("File created: " + dir.getName());
+		      }
+		    } catch (IOException e) {
+		    	System.out.println("An error occurred.");
+		    	e.printStackTrace();
+		    }
+		 
+		 try {
+			 FileWriter logWriter = new FileWriter(this.path + date.getDay() + "-" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + FILE_EXTENSION);
+			 logWriter.write(sKey1 + prefix + sKey2 + args + "\u001B[0m");
+			 logWriter.close();
+			 System.out.println("Successfully wrote to the file.");
+		 } catch (IOException e) {
+		    	System.out.println("An error occurred during the wrote to the file.");
+		    	e.printStackTrace();
+		 }
 		
 	}
 	
@@ -59,6 +106,14 @@ public class SuperLogger {
 		return args;
 	}
 	
+	public AnsiConsoleTextColors getColor() {
+		return aColor;
+	}
+	
+	public String getLogFilePath() {
+		return path;
+	}
+	
 	public void setPrefix(String newPrefix) {
 		this.prefix = newPrefix;
 	}
@@ -69,6 +124,14 @@ public class SuperLogger {
 	
 	public void setArgs(String newArgs) {
 		this.args = newArgs;
+	}
+	
+	public void setColor(AnsiConsoleTextColors newAColor) {
+		this.aColor = newAColor;
+	}
+	
+	public void setNewLogFilePath(String newPath) {
+		this.path = newPath;
 	}
 
 }
---
+++ SuperLoggerApi/src/fr/galaglow/superloggerapi/utils/AnsiConsoleTextColors.java
@@ -0,0 +1,33 @@
+package fr.galaglow.superloggerapi.utils;
+
+public enum AnsiConsoleTextColors {
+	
+	BASIC(""),
+	
+	BLACK("\u001B[30m"),
+	
+	RED("\u001B[31m"),
+	
+	GREEN("\u001B[32m"),
+	
+	YELLOW("\u001B[33m"),
+	
+	 BLUE("\u001B[34m"),
+	
+	 PURPLE("\u001B[35m"),
+	
+	CYAN("\u001B[36m"),
+	
+	WHITE( "\u001B[37m");
+	
+	private final String ansiColor;
+
+	AnsiConsoleTextColors(String aColor) {
+		this.ansiColor = aColor;
+	}
+	
+	public String getAnsiColor() {
+        return ansiColor;
+    }
+
+}
